22904fb1fc39d6c6c00d1cf36bf0b7d96f178569
Sohan Maity <sohanmaity5272@gmail.com>
2026-06-30 23:59:26 +0530

    password hassing

--- src/controllers/userAuthenticate.js
+++ src/controllers/userAuthenticate.js
@@ -1,10 +1,13 @@
-const User = require("./models/user")
-const validate = require("./utils/validator")
+const User = require("./models/user");
+const validate = require("./utils/validator");
+const bcrypt = require("bcrypt")
 
 const register = async (req,res)=>{
     try{
           
         validate(req.body)
+        const {password} = req.body;
+        req.body.password = bcrypt.hash(password,10);
        const user =  User.create(req.body)
     }
 
