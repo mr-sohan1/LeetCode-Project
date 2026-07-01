6783388ad6ba219bf91a190f681ffc495cf44c32
Sohan Maity <sohanmaity5272@gmail.com>
2026-07-01 07:56:51 +0530

    register

--- src/controllers/userAuthenticate.js
+++ src/controllers/userAuthenticate.js
@@ -1,6 +1,7 @@
 const User = require("./models/user");
 const validate = require("./utils/validator");
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 const register = async (req,res)=>{
     try{
@@ -8,7 +9,12 @@ const register = async (req,res)=>{
         validate(req.body)
         const {password} = req.body;
         req.body.password = bcrypt.hash(password,10);
-       const user =  User.create(req.body)
+       
+        const user = await User.create(req.body)
+
+        const token = jwt.sign({id:user._id,emailID: user.emailID},process.env.JWT_KEY, { expiresIn: "7d" })
+        res.cookie("token",token,{maxAge:7*3600*1000});
+        res.status(200).send("Register Sucessfully");
     }
 
     catch(err){
